e7d63ebd0b4f8c001692fc9641b50bcaf902d89c
Subhaneel Agarwalla <a.subhaneel@gmail.com>
2021-03-25 13:16:20 +0530

    first commit

---
+++ mqtt_explorer_application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mqtt_explorer_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
